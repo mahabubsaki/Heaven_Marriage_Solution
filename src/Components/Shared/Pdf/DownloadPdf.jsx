36a1c02dbacbaf2ccb8cd313d969179be3c45c2b
mahabubsaki <md929820@gmail.com>
2025-06-19 16:02:09 +0600

    added loading state

--- src/Components/Shared/Pdf/DownloadPdf.jsx
+++ src/Components/Shared/Pdf/DownloadPdf.jsx
@@ -10,10 +10,12 @@ const DownloadPdf = ({ formData }) => {
 
             {({ loading, error }) => (
                 <button
-
-                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                    disabled={loading}
+                    className="px-4 py-2 bg-blue-600 flex items-center gap-3 text-white rounded hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
-                    Download PDF
+                    {loading ? 'Preparing Document' : 'Download PDF'}
+                    {loading ? <span className='animate animate-pulse rounded-full size-[15px] bg-green-500 inline-block' /> : null}
+                    {error && <span className="text-red-500">Error: {error.message}</span>}
                 </button>
             )}
         </PDFDownloadLink>
